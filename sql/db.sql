20b0223c7d6344a18febadd1e81ecc992456a33a
Sergey Grigoriev <sergey@grigoriev.eu>
2015-11-20 16:53:44 +0100

    sql schema

---
+++ sql/db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+  id         INT         NOT NULL AUTO_INCREMENT,
+  username   VARCHAR(50) NOT NULL,
+  password   VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name  VARCHAR(50) NOT NULL,
+  email      VARCHAR(80) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX (email)
+);
