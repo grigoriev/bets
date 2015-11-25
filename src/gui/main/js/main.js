e8287226067e74ff9867439248c50061ae15aef8
Sergey Grigoriev <sergey@grigoriev.eu>
2015-11-25 22:05:44 +0100

    menu

--- src/gui/main/js/main.js
+++ src/gui/main/js/main.js
@@ -9,3 +9,22 @@ function doLogout() {
     $.removeCookie('session-ip-address');
     window.location.replace('/login');
 }
+
+$(document).ready(function () {
+    $('#cssmenu').find('li.has-sub>a').on('click', function () {
+        $(this).removeAttr('href');
+        var element = $(this).parent('li');
+        if (element.hasClass('open')) {
+            element.removeClass('open');
+            element.find('li').removeClass('open');
+            element.find('ul').slideUp();
+        } else {
+            element.addClass('open');
+            element.children('ul').slideDown();
+            element.siblings('li').children('ul').slideUp();
+            element.siblings('li').removeClass('open');
+            element.siblings('li').find('li').removeClass('open');
+            element.siblings('li').find('ul').slideUp();
+        }
+    });
+});
